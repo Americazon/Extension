800bb73ddb8cf177781bba18a094e18821412cae
Austin Blanchard <42254605+Nitsua365@users.noreply.github.com>
2023-06-18 20:00:29 -0500

    added picture, title, and COO to popup.html list

--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,33 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.result) {
 
-    let elem = document.getElementById("result");
+    const data = Object.values(message.result)
+
+    let elem = document.getElementById("result")
+    let listDiv = document.createElement("div")
+
+    data.forEach(productData => {
+
+      let itemDiv = document.createElement("div")
+
+      let titeElem = document.createElement("h1")
+      let cooElem = document.createElement("h3")
+      let imageElem = document.createElement("img")
+
+      titeElem.innerText = productData?.productName || ""
+      cooElem.innerText = productData?.COO || ""
+      imageElem.src = productData?.productImage || ""
+      
+      // create title
+      itemDiv.appendChild(titeElem)
+      itemDiv.appendChild(cooElem)
+      itemDiv.appendChild(imageElem)
+
+      // append list item
+      listDiv.appendChild(itemDiv)
+    })
+
+    elem.appendChild(listDiv);
   
-    elem.textContent = JSON.stringify(message.result, undefined, 2);
   }
 });
